3dae1fba879422c2c48d280fcb6e6de026ac236e
Elena Crenguta Lindqvist <crenguta@gmail.com>
2019-08-16 12:10:15 +0200

    argsparse

--- mens.py
+++ mens.py
@@ -4,13 +4,15 @@ import re
 
 # How to run mens.py
 parser = argparse.ArgumentParser()
-parser.add_argument("list", help="Type in your previous mens days separated by coma e.g. 6.12.2018,2.1.2019,27.1.2019,23.2.2019,17.4.2019")
+parser.add_argument("list", help="Type in your previous mens days separated by coma e.g. 6.12.2018,2.1.2019,27.1.2019,23.2.2019,17.4.2019", type=str)
 #parser.add_argument("date", help="Type in the date of your last mentruation and the menstrual cycle lenght, separated by coma e.g. 23.2.2019,26")
 args = parser.parse_args()
 
-# print (args.list)
+print (args.list)
 
-re.split("\,", args)
+argssplit= args.list.split(",")
+
+print (argssplit)
 
 #for element in argssplit:
 #    print(element)
