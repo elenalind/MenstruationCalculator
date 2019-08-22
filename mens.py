e1a9c2196da8db47ef7b464905287b490b2b34cd
Elena Crenguta Lindqvist <crenguta@gmail.com>
2019-08-22 09:40:48 +0200

    calculates next menstruation, not optimized

--- mens.py
+++ mens.py
@@ -1,9 +1,10 @@
 import argparse
-from datetime import datetime
+import time
+from datetime import datetime, date, time, timedelta
 
 # How to run mens.py
 parser = argparse.ArgumentParser()
-parser.add_argument("list", help="Type in your previous mens days separated by coma e.g. 6.12.2018,2.1.2019,27.1.2019,23.2.2019,17.4.2019", type=str)
+parser.add_argument("list", help="Type in your previous mens days separated by coma e.g. 6.12.2018,2.1.2019,27.1.2019,23.2.2019,20.3.2019,17.4.2019,11.5.2019,5.6.2019,3.7.2019,30.7.2019", type=str)
 #parser.add_argument("date", help="Type in the date of your last mentruation and the menstrual cycle lenght, separated by coma e.g. 23.2.2019,26")
 args = parser.parse_args()
 
@@ -11,8 +12,20 @@ print ("Your dates", args.list)
 
 argssplit= args.list.split(",")
 
+print ("Your cycle lenths are:")
+sum_days = 0 
 for item, next_item in zip(argssplit, argssplit[1:]):
-    print (abs((datetime.strptime(item, '%d.%m.%Y'))-(datetime.strptime(next_item, '%d.%m.%Y'))).days)
+    cyclelength = ((datetime.strptime(next_item, '%d.%m.%Y'))-(datetime.strptime(item, '%d.%m.%Y'))).days
+    sum_days = sum_days + cyclelength
+    print (cyclelength)
+
+print ("Your average cycle length is:", int(sum_days / (len(argssplit) - 1)))
+
+last_mens = datetime.strptime(argssplit[len(argssplit)-1], '%d.%m.%Y')
+next_mens = last_mens + timedelta(days=cyclelength)
+print ("Your next menstrutation will probably be on:", next_mens)
+
+
 
 # Throw Warning if more than 30 days between mens days. The user can doublecheck the input.
 # Throw Warning if for option two, the period lenght is too long, >30 days
